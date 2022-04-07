30702bf735ef77321cef15b92c54f432ead7a763
Le Quang Thang <lethang7794@gmail.com>
2022-04-07 20:43:06 +0700

    Ex 4 - Extra 1: put cache in context

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -8,6 +8,7 @@ import {
   PokemonForm,
   PokemonDataView,
   PokemonErrorBoundary,
+  usePokemonResource,
 } from '../pokemon'
 import {createResource} from '../utils'
 
@@ -40,6 +41,11 @@ function getPokemonResource(name) {
   return pokemonResource
 }
 
+const PokemonResourceCacheContext = React.createContext(getPokemonResource)
+function usePokemonResourceCache() {
+  return React.useContext(PokemonResourceCacheContext)
+}
+
 function createPokemonResource(pokemonName) {
   return createResource(fetchPokemon(pokemonName))
 }
@@ -48,6 +54,7 @@ function App() {
   const [pokemonName, setPokemonName] = React.useState('')
   const [startTransition, isPending] = React.useTransition(SUSPENSE_CONFIG)
   const [pokemonResource, setPokemonResource] = React.useState(null)
+  const getPokemonResource = usePokemonResourceCache()
 
   React.useEffect(() => {
     if (!pokemonName) {
@@ -57,7 +64,7 @@ function App() {
     startTransition(() => {
       setPokemonResource(getPokemonResource(pokemonName))
     })
-  }, [pokemonName, startTransition])
+  }, [getPokemonResource, pokemonName, startTransition])
 
   function handleSubmit(newPokemonName) {
     setPokemonName(newPokemonName)
